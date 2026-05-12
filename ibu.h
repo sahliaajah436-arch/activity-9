56d521ceab3311bfd04adee9546ec002762e7d80
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-05-12 18:47:02 +0700

    membuat class ibu

--- ibu.h
+++ ibu.h
@@ -6,3 +6,21 @@
 #include "anak.h"
 
 #include <vector>
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
+
